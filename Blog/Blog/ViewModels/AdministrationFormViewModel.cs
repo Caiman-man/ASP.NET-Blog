e72f55a5d1b72e4152b75feef8d99938d266c4ae
Alexander Ivanov <82727730+Caiman-man@users.noreply.github.com>
2022-05-28 18:39:15 +0300

    add posts design

--- Blog/Blog/ViewModels/AdministrationFormViewModel.cs
+++ Blog/Blog/ViewModels/AdministrationFormViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using Blog.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Blog.ViewModels
 {
